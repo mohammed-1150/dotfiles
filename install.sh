49d0182b52be2a1157ad34f728cf7bf47f6c08dc
mohammed-1150 <90805205+mohammed-1150@users.noreply.github.com>
2023-02-14 05:01:08 +0000

    more fixes

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ script_dir=$(dirname "$(readlink -f "$0")")
 rm -rf $HOME/.bash_profile
 rm -rf $HOME/.bashrc
 
-ln -s $PWD/bash_profile $HOME/.bash_profile
-ln -s $PWD/bashrc $HOME/.bashrc
+ln -s $script_dir/bash_profile $HOME/.bash_profile
+ln -s $script_dir/bashrc $HOME/.bashrc
